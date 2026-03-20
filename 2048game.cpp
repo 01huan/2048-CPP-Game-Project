a28580e80334d6d78896d4e68a6047af856f97ad
01huan <ethan20080205@gmail.com>
2026-03-20 16:31:45 +0800

    add 加註解

--- 2048game.cpp
+++ 2048game.cpp
@@ -6,6 +6,7 @@ vector<vector<int>> board(4, vector<int>(4, 0));
 vector<char> cmd = {'w', 'a', 's', 'd', 'q', 'W', 'A', 'S', 'D', 'Q'};
 int act=0, points=0, overgoal=0, playing=1;
 
+//輸出遊戲盤面
 void print() {
     for (int i=0; i<4; i++) {
         cout << "|";
@@ -17,9 +18,11 @@ void print() {
     }
 }
 
+//隨機產生數字2或4（依照自己之前的遊戲經驗，出現2的機率比4大，因此目前假設出現2和4的的機率比為4:1）
 void newBlock() {
     vector<int> init = {2,2,2,2,4};
     vector<pair<int,int>> empty_coord;
+    //檢查是否有空白格子
     for (int i=0; i<4; i++) {
         for (int j=0; j<4; j++) {
             if (board[i][j] == 0) empty_coord.push_back({i,j});
@@ -33,6 +36,7 @@ void newBlock() {
     board[x][y] = init[rand() % 5];
 }
 
+//檢查是否還能移動，用於偵測遊戲進度
 bool movable() {
     for (int i=0; i<4; i++) {
         for (int j=0; j<4; j++) {
@@ -44,27 +48,28 @@ bool movable() {
     return false;
 }
 
+//依照指令移動
 bool move(char c) {
-    vector<vector<int>> last = board;
+    vector<vector<int>> last = board;   //用於檢查移動方向是否有效
 
     //向上
     if (c == 'w') {
         for (int j=0; j<4; j++) {
-            vector<int> temp(4, 0);
+            vector<int> temp(4, 0); //將原本該排數字抽出獨立處理
             int k=0, combined=0;
             for (int i=0; i<4; i++) {
-                if (board[i][j] != 0) {
-                    if (k>0 && !combined && temp[k-1]==board[i][j]) {
-                        temp[k-1] *= 2;
-                        points += temp[k-1];
-                        combined = 1;
+                if (board[i][j] != 0) { //取出非0數字
+                    if (k>0 && !combined && temp[k-1]==board[i][j]) {   //檢查是否達合併條件
+                        temp[k-1] *= 2; //合併
+                        points += temp[k-1];    
+                        combined = 1;   //標記已經合併，以免下一個數字相同而重複合併
                     } else {
                         temp[k++] = board[i][j];
                         combined = 0;
                     }
                 }
             }
-            for (int i=0; i<4; i++) board[i][j] = temp[i];
+            for (int i=0; i<4; i++) board[i][j] = temp[i];  //寫回borad
         }
     }
 
@@ -134,6 +139,7 @@ bool move(char c) {
     return (last != board);
 }
 
+//檢查是否已經合併出2048
 bool achieve() {
     for (const auto & row : board) {
         for (int block : row) if (block == 2048) return true;
@@ -142,6 +148,7 @@ bool achieve() {
 }
 
 int main() {
+    //初始化遊戲盤面
     srand(time(0));
     newBlock();
     newBlock();
@@ -149,6 +156,7 @@ int main() {
     while (playing) {
         if (!movable()) break;
 
+        //在達標時詢問玩家是否繼續
         if (!overgoal && achieve()) {
             system("cls");
             cout << "You Win!\nYou got " << points << " points in " << act << " moves.\n";
@@ -159,11 +167,13 @@ int main() {
             overgoal = 1;
         }
         
+        //每一次的移動提示訊息
         system("cls");
         cout << "Moves: " << act << "\tPoints: " << points << "\n";
         print();
         cout << "Tap WASD to move or tap Q to quit: ";
 
+        //偵測移動方向
         int input = _getch();
         char c = ' ';
 
